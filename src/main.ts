9c14fd5175ca115374578a2c28e10c393839bf47
AaronTu <ken888686@gmail.com>
2025-12-16 21:46:18 +0900

    feat: Add Swagger API documentation and configured a global API prefix to exclude health check routes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs E-commerce API')
+    .setDescription('This is the Nestjs E-commerce API')
+    .setVersion('1.0')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
